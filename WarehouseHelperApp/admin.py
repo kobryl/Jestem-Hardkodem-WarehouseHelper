4f5d76a4d5e2a25f51583df621787e8bc4e88d2f
kobryl <59873325+kobryl@users.noreply.github.com>
2024-01-10 15:01:06 +0100

    Initial view, css and js directories, database models

--- WarehouseHelperApp/admin.py
+++ WarehouseHelperApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from WarehouseHelperApp import models
+
 # Register your models here.
+
+admin.site.register(models.Product)
+admin.site.register(models.OrderItem)
+admin.site.register(models.ProductLocation)
--- WarehouseHelperApp/models.py
+++ WarehouseHelperApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    weight = models.IntegerField()
+    length = models.IntegerField()
+    width = models.IntegerField()
+    height = models.IntegerField()
+
+    def __str__(self):
+        return self.name + f", weight: {self.weight} kg, size [m]: {self.length}x{self.width}x{self.height}"
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return  self.product.name + f" {self.quantity}"
+
+
+class ProductLocation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.location
